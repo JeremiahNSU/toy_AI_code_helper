8468600281d144391bf88a7805d1cfb3fa430f67
JeremiahNSU <michealnsubejerry@gmail.com>
2025-12-31 14:58:23 +1100

    B: Added an argparse thingy that reads CL input for the AI agent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import argparse
 
 def main():
     load_dotenv()
@@ -9,15 +9,19 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=args.user_prompt,
     )
 
     if response.usage_metadata is None:
         raise RuntimeError("No usage metadata found in the response")
-    print("User prompts used: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"User prompts used: {args.user_prompt}.")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response text: {response.text}")
