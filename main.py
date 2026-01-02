bdbbcc658ae28bcc9f4ae57ace3cd6f51bff30cd
JeremiahNSU <michealnsubejerry@gmail.com>
2026-01-02 12:23:47 +1100

    D: file with function for overwriting files is created

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+from urllib import response
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
+
 
 def main():
     load_dotenv()
@@ -11,20 +14,28 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
 
     if response.usage_metadata is None:
         raise RuntimeError("No usage metadata found in the response")
-    print(f"User prompts used: {args.user_prompt}.")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response text: {response.text}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}.")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+    
 
 
 if __name__ == "__main__":
