e232493e8c5e3007c8c513a7861f6bfcffe47b47
JeremiahNSU <michealnsubejerry@gmail.com>
2026-01-04 16:33:07 +1100

    L: The AI has more access to more functions yay!

--- functions/call_function.py
+++ functions/call_function.py
@@ -14,8 +14,58 @@ schema_get_files_info = types.FunctionDeclaration(
         },
     ),
 )
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the content of a file located at `file_path`, ensuring it is within `working_directory`",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read, relative to the working directory",
+            ),
+        }, required=["file_path"]
+    ), 
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file located at `file_path`, ensuring it is within `working_directory`",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read, relative to the working directory",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="List of arguments to pass to the Python script",
+                items=types.Schema(type=types.Type.STRING),
+            ),
+        },required=["file_path"]
+    ),
+)
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes content to a file located at `file_path`, ensuring it is within `working_directory`",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to write, relative to the working directory",
+                
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to write to the file",
+            ),
+        },required=["file_path", "content"]
+    ),
+    )
 
 available_functions = types.Tool(
-    function_declarations=[schema_get_files_info],
+    function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file],
 )
 
