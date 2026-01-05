3934819ed49feb1c972871f8c377c97e43e98304
JeremiahNSU <michealnsubejerry@gmail.com>
2026-01-06 01:46:02 +1100

    N: add agent loop with conversation history and tool call feedback

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 from google.genai import types
 from prompts import system_prompt
 from call_function import *
-
+import sys
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,36 +18,39 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    messeges = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-
-
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-    tools=[available_functions], system_instruction=system_prompt
-)
-    )
-
-    if response.usage_metadata is None:
-        raise RuntimeError("No usage metadata found in the response")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}.")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-
-
+    previous_responses = []
     
-    if response.function_calls is None:
-        print("Response:")
-        print(response.text)
-    else:
-        function_results = []
+    client = genai.Client(api_key=api_key)
+    for i in range(20): 
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messeges,
+            config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+        )
 
-        for function_call in response.function_calls:
+        if response.usage_metadata is None:
+            raise RuntimeError("No usage metadata found in the response")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}.")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        for candidate in response.candidates:
+            messeges.append(types.Content(role="assistant", parts=candidate.content.parts))
+        if not response.function_calls:
+            print("Response:")
+            print(response.text)
+            return 
+        else:
+            function_results = []
+
+
+        for function_call in response.function_calls:             
+
             function_call_result = call_function(function_call, verbose=args.verbose)
 
             if not function_call_result.parts:
@@ -56,11 +59,15 @@ def main():
                 raise Exception("Function call returned no response")
             if function_call_result.parts[0].function_response.response is None:
                 raise Exception("Function call returned no result field in response")
-
             function_results.append(function_call_result.parts[0])
+            
 
             if args.verbose:
                 print(f"-> {function_call_result.parts[0].function_response.response}")
+        messeges.append(types.Content(role="user", parts=function_results))     
+    
+    print("Max iterations reached.")
+    sys.exit(1)
 
 
 
